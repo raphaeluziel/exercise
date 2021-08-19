3d2cc66ee14f7f6eb067da357857abf405dbcfe7
raphaeluziel <raphaeluziel@hotmail.com>
2021-08-19 11:43:47 -0400

    move to digital ocean

--- public/client.js
+++ public/client.js
@@ -1,39 +1,39 @@
 $(document).ready(function() {
-  
+
   // A nicer display of user logs than the json one
   $("#getLogs").on("click", function() {
-  
+
     var link = "";
-    
+
     // Get the information from the form
     var i = 1;
     var userId = document.getElementById("userId").value;
     var from = document.getElementById("from").value;
     var to = document.getElementById("to").value;
-    var limit = document.getElementById("limit").value;  
+    var limit = document.getElementById("limit").value;
 
-    // Create a link to the api that gets all the information 
+    // Create a link to the api that gets all the information
     // (the one that the server.js returns as a json)
-    link = "https://api-uziel-exercise.glitch.me/api/exercise/log?userId=" + userId;
+    link = "api/exercise/log?userId=" + userId;
     if (from) {link += "&from=" + from}
     if (to) {link += "&to=" + to}
     if (limit) {link +=  "&limit=" + limit}
-    
+
     // Make the link clickable if user prefers the json format
     document.getElementById("urlGenerated").innerHTML = link;
     document.getElementById("urlGenerated").href = link;
- 
+
     // Get the data and generate a nice looking table of it
     $.getJSON(link, function(json) {
-      
+
       var html;
-      
+
       // If data is received draw the table and populate it with the data
       if (!json.message){
         // Table title with basic information to be displayed
-        html = "<p><strong>Activity log for " + json.username + " from " + json.from + " to " + json.to + " limit " + json.count 
+        html = "<p><strong>Activity log for " + json.username + " from " + json.from + " to " + json.to + " limit " + json.count
           + "</strong></p><table><tr><th>No</th><th>DATE</th><th>ACTIVITY</th><th>DURATION</th></tr>";
-        
+
         // Table display
         json.log.forEach(function(x){
           html += "<tr><td>" + i + "</td><td>" + x.date + "</td><td>" + x.description + "</td><td>" + x.duration + "</td></tr>";
@@ -41,26 +41,26 @@ $(document).ready(function() {
         })
         $("#activityLog").html(html);
         $("#errorMessage").html("");
-      }  
+      }
       else{
         $("#errorMessage").html(json.message);
         $("#activityLog").html("");
       }
-      
+
       i = 1;
       html = "";
     });
-    
+
   });
-  
-  
+
+
   // Get a list of all users, but as a nice looking list instead of a json object
   var toggle = -1;
-  
+
   $("#getAllUsers").on("click", function() {
- 
-    $.getJSON("https://api-uziel-exercise.glitch.me/api/exercise/users", function(json) {
-      
+
+    $.getJSON("api/exercise/users", function(json) {
+
       // Toggle showing and hiding the user list
       if (toggle < 0){
         var html = "<ol>";
@@ -72,11 +72,11 @@ $(document).ready(function() {
         html += "</ol>"
 
         $("#userList").html(html);
-      }   
+      }
       else{
         $("#userList").html("");
       }
-      
+
       toggle *= -1;
     });
   });
--- server.js
+++ server.js
@@ -41,7 +41,7 @@ var userModel = mongoose.model('userModel', userSchema);
 
 // Search for and add user
 app.route('/api/exercise/new-user').post(function(req, res){
-  
+
   if (!req.body.username) {return res.send("No username provided");}
 
   // Search the database for the user
@@ -55,34 +55,34 @@ app.route('/api/exercise/new-user').post(function(req, res){
     }
     else{res.json({"message": "already in database"});}
   });
-  
+
 });
-  
+
 
 // Add activities
 app.route('/api/exercise/add').post(function(req, res){
-  
+
   var date;
-  
+
   // If user does not provide a date, default to current date
   req.body.date ? date = new Date(req.body.date) : date = new Date();
-  
+
   // Errors if user leaves out fields, or uses an invalid date format
   if (!req.body.userId) {return res.send("No user ID provided");}
   if (!req.body.description) {return res.send("No description provided");}
   if (!req.body.duration) {return res.send("No duration provided");}
   if (date == "Invalid Date") {return res.send("Not a valid date format");}
-  
+
   // If user provides correct information, then search for the user
   userModel.findById(req.body.userId, function(err, data){
 
     if (err) return console.log("ERROR FINDING BY ID");
-    
+
     if(!data) {return res.send("No user with that ID exists")}
-    
+
     // If user is found in database, add activity to the array
     data.activity.push({date: req.body.date, description: req.body.description, duration: req.body.duration});
-    
+
     // Save updated array to the database
     data.save(function(err, updatedActivity){
       if(err) return console.log("ERROR SAVING ACTIVITY");
@@ -100,78 +100,78 @@ app.route('/api/exercise/add').post(function(req, res){
 
 // Show exercise log
 app.route('/api/exercise/log').get(function(req, res){
-  
+
   var from, to;
-  
+
   // If dates are left blank default to 1970 (javascript 0 time) for the from field
   // and/or the current date for the to field
   (!req.query.from) ? from = new Date(0) : from = new Date(req.query.from);
   (!req.query.to) ? to = new Date() : to = new Date(req.query.to);
-  
+
   if ((from == "Invalid Date") || (to == "Invalid Date")) {return res.json({message: "Not a valid date format"})}
- 
+
   // Find user
   var query = userModel.findById(req.query.userId, function(err, doc){
-    
+
     if (err) return console.log("ERROR FINDING BY ID");
-    
+
     if(!doc) {return res.json({message: "No user with that ID exists"})}
-    
+
     var log = [];
 
     // Show maximum of user limit or number of records
     var len;
     req.query.limit > doc.activity.length ? len = doc.activity.length : len = req.query.limit;
-    
+
     // Generate an array of all activities fitting the constraints
     for (var i = 0; i < doc.activity.length; i++){
       log.push({"date": doc.activity[i].date, "description": doc.activity[i].description, "duration": doc.activity[i].duration});
     }
-    
+
     // Sort the array by date
     log.sort((a, b) => b.date - a.date);
-    
+
     // Filter out dates outside of user provided date limits
     log = log.filter(d => d.date >= from && d.date <= to);
-    
-    // If user provides a limit of activities to show, then 
+
+    // If user provides a limit of activities to show, then
     // slice off the correct amount from the end of the array
     if(req.query.limit > 0){
       log = log.slice(0, req.query.limit);
     }
-    
+
     // Format the date objects for a nicer display
     log.forEach(function(d){
       d.date = d.date.toDateString();
     });
-    
+
     // Return a json response of the requested information
     res.json({_id: doc._id, username: doc.username, from: from.toDateString(), to: to.toDateString(), count: log.length ,log: log});
-  }) 
+  })
 });
 
 
 // Get a list of all users in the database
 app.route('/api/exercise/users').get(function(req, res){
-  
+
   // Select all users
   userModel.find({}, function(err, doc){
-    
+
     if (err) return console.log("ERROR FINDING ALL USERS");
-    
+
     if(!doc) {return res.json({message: "Database is empty"})}
-    
+
     var users = [];
-    
+
     // Create an array of usernames and corresponding id's
     for (var i = 0; i < doc.length; i++){
       users[i] = {"username": doc[i].username, "userId": doc[i]._id}
     }
-    
+
     // Return this array of objects
     res.send(users);
   })
-  
+
 });
 
 
